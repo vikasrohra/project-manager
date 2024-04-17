bf339b53c2e84c03510800a36ee86bb167ed50f2
vikasrohra <rohra-vikas@aramark.com>
2024-04-17 22:31:04 +0530

    Rendered form via object oriented way using TS

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        // 2nd parameter, asks to to import with deep clone or not
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const projectInput = new ProjectInput();
