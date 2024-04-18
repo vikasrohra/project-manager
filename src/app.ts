e6f6dbd009b5a153d301a500b7d511d9cab298d7
vikasrohra <rohra-vikas@aramark.com>
2024-04-18 20:42:33 +0530

    Interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
 
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
@@ -10,9 +14,25 @@ class ProjectInput {
         // 2nd parameter, asks to to import with deep clone or not
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+ 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
