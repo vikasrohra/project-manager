cac91fa184bbf81c67ee1a2be7aae329af0dcb4d
vikasrohra <rohra-vikas@aramark.com>
2024-04-19 22:52:57 +0530

    Rendering project items with a class and added a getter for showing no of people

--- src/app.ts
+++ src/app.ts
@@ -121,6 +121,37 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    get persons() {
+        if(this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {
+        
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
@@ -159,9 +190,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';
         for(const prjItem of this.assignedProjects) {
-            const listtem = document.createElement('li');
-            listtem.textContent = prjItem.title;
-            listEl.appendChild(listtem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }      
 }
