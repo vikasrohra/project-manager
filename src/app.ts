b6861144892c1574a4d54d6b76826ed11000fe71
vikasrohra <rohra-vikas@aramark.com>
2024-04-21 14:45:01 +0530

    Finished Drag & Drop feature

--- src/app.ts
+++ src/app.ts
@@ -50,6 +50,18 @@ class ProjectState extends State<Project> {
     addProject(title: string, description: string, noOfPeople: number) {
         const project = new Project(Math.random().toString(), title, description, noOfPeople, ProjectStatus.Active);
         this.projects.push(project);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if(project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for(const listenerFn of this.listeners) {// This will trigger the project list class that somthing is updated in the projects
             listenerFn(this.projects.slice()); // Return the brand new copy of the projects
         }
@@ -198,8 +210,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }        
     }
 
+    @Autobind
     dropHandler(event: DragEvent) {
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @Autobind
