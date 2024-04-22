8c7d0e214c9ad496eccd215985ca28c3b431fb95
vikasrohra <rohra-vikas@aramark.com>
2024-04-23 00:35:36 +0530

    Installed 3rd party packages in TS for reducing manually written code

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
+import _ from 'lodash';
 
 const projectInput = new ProjectInput();
 const activePrjList = new ProjectList("active");
 const finishedPrjList = new ProjectList("finished");
+
+
+console.log(_.shuffle([1, 2, 3]));
